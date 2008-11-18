c164a385de807798cd25ff3a8c0552530de5616a
Andrew Neil <andrew.jr.neil@gmail.com>
2008-11-18 18:13:08 +0000

    Spec'ed filename method for TextAsset.

--- spec/lib/text_asset_extensions_spec.rb
+++ spec/lib/text_asset_extensions_spec.rb
@@ -37,4 +37,67 @@ describe TextAsset do
     @model.should respond_to(:filename)
   end
   
+  # ----------------
+  describe "filename" do
+    
+    it "should use name with default extension" do
+      class << @model
+        attr_accessor :name
+      end
+      @model.name = "hit me"
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/hit me.html"
+    end
+  
+    it "should use record name, which may include its own extension" do
+      class << @model
+        attr_accessor :name
+      end
+      @model.name = "myfile.txt"
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/myfile.txt.html"
+    end
+  
+    it "should use custom filter as extension" do
+      class << @model
+        attr_accessor :name, :filter_id
+      end
+      @model.name = "hit me"
+      @model.filter_id = "txt"
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/hit me.txt"
+    end
+  
+    it "should include .min when minified" do
+      class << @model
+        attr_accessor :name, :minify
+      end
+      @model.name = "myfile"
+      @model.minify = true
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/myfile.min.html"
+    end
+  
+    it "should not include .min when not minified" do
+      class << @model
+        attr_accessor :name, :minify
+      end
+      @model.name = "myfile"
+      @model.minify = false
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/myfile.html"
+    end
+  
+    it "should use name(.min).extension when provided with all 3" do
+      class << @model
+        attr_accessor :name, :filter_id, :minify
+      end
+      @model.name = "myfile"
+      @model.filter_id = "markdown"
+      @model.minify = true
+      @model.filename.should == "#{RAILS_ROOT}/design/mock_models/myfile.min.markdown"
+    end
+  end
+  
+  describe "load_file" do
+    it "should description" do
+      
+    end
+  end
+  
 end
